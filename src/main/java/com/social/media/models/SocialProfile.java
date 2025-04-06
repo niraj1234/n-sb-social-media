bf401d4e2b9218bef7d43d4788ad68b2f9cdc1a9
niraj1234 <niraj.webtech@gmail.com>
2025-04-06 11:35:24 +0530

    06_APR_2025_11:20 | Social Media all entity and project structure implemented vid-131

---
+++ src/main/java/com/social/media/models/SocialProfile.java
@@ -0,0 +1,22 @@
+package com.social.media.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SocialProfile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+//    @OneToOne(mappedBy = "socialProfile")
+    @OneToOne
+    @JoinColumn(name="social_user")
+    private SocialUser user;
+
+}
